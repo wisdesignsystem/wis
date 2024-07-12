9de094f256d5767af94d34bc0356ff73476aa4ba
FaureWu <faure@wis.design>
2024-07-12 15:30:37 +0800

    chore(develop): commitlint and branch name validate

--- src/examples/button/Button.page.jsx
+++ src/examples/button/Button.page.jsx
@@ -1,13 +1,7 @@
-// import Button from 'remote:demo/Button'
 import Button from 'remote:self/Button'
 
 function Example() {
-  return (
-    <>
-      22222332342323243243223423343
-      <Button>221122222111333</Button>
-    </>
-  )
+  return <Button></Button>
 }
 
 export default Example
