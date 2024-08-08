7d06e26bc79317055ab1ed4886b49e2ce45c86dc
FaureWu <faure@wis.design>
2024-08-08 22:11:04 +0800

    feat(style): support the global wis style import

--- .wisrc.js
+++ .wisrc.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url)
+
 export default {
   // 项目类型
   mode: 'library',
@@ -13,9 +17,12 @@ export default {
 
   // 项目导出的共享资源
   exposes: {
+    './style': '@/packages/style/index.js',
     './Icon': '@/packages/icon/index.js',
     './Button': { pc: '@/packages/button/pc/Button', mobile: '@/packages/button/mobile/Button' },
   },
 
-  plugins: ['@wisdesign/plugin-less'],
+  plugins: [require.resolve('@wisdesign/plugin-less')],
+
+  remoteEntry: './style',
 }
--- src/packages/button/pc/Button.jsx
+++ src/packages/button/pc/Button.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
+
 import styles from './Button.module.less'
 
 function Button() {
+  const [count, setCount] = useState(0)
+
   return (
-    <button className={styles.button} onClick={() => window.alert('pc button click')}>
-      PC Button
+    <button
+      className={styles.button}
+      onClick={() => {
+        window.alert('pc button click')
+        setCount(count + 1)
+      }}
+    >
+      PC Button {count}
     </button>
   )
 }
