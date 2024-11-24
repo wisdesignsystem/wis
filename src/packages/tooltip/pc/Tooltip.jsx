b7feaba23ebdf759316d462c49bae129b886c5a4
FaureWu <faure@wis.design>
2024-11-24 10:29:48 +0800

    feat(tooltip): create the tooltip component

---
+++ src/packages/tooltip/pc/Tooltip.jsx
@@ -0,0 +1,76 @@
+import { Children } from 'react'
+import PropTypes from 'prop-types'
+import * as RDXTooltip from '@radix-ui/react-tooltip'
+
+import styles from './Tooltip.module.scss'
+
+function Tooltip({ className, side = 'top', align = 'center', text, open, defaultOpen, onOpen, children }) {
+  return (
+    <RDXTooltip.Provider skipDelayDuration={300}>
+      <RDXTooltip.Root open={open} defaultOpen={defaultOpen} delayDuration={500} onOpenChange={onOpen}>
+        <RDXTooltip.Trigger asChild>{Children.only(children)}</RDXTooltip.Trigger>
+        <RDXTooltip.Portal>
+          <RDXTooltip.Content className={styles.popper} side={side} align={align}>
+            {text}
+            <RDXTooltip.Arrow fill="currentColor" className={styles.arrow}></RDXTooltip.Arrow>
+          </RDXTooltip.Content>
+        </RDXTooltip.Portal>
+      </RDXTooltip.Root>
+    </RDXTooltip.Provider>
+  )
+}
+
+Tooltip.displayName = 'Tooltip'
+Tooltip.propTypes = {
+  /**
+   * @hidden
+   */
+  className: PropTypes.string,
+
+  /**
+   * The preferred side of the trigger to render against when open. May change when collisions occur.
+   */
+  side: PropTypes.oneOf(['top', 'right', 'bottom', 'left']),
+
+  /**
+   *  The preferred alignment against the trigger. May change when collisions occur.
+   */
+  align: PropTypes.oneOf(['start', 'center', 'end']),
+
+  /**
+   * The content to render in the tooltip.
+   */
+  text: PropTypes.string.isRequired,
+
+  /**
+   * The controlled open state of the tooltip.
+   */
+  open: PropTypes.bool,
+
+  /**
+   * The open state of the tooltip when it is initially rendered.
+   */
+  defaultOpen: PropTypes.bool,
+
+  /**
+   * @hidden
+   */
+  children: PropTypes.node,
+
+  /**
+   * Event handler called when the open state of the tooltip changes.
+   *
+   * @example
+   *
+   * function handleOpen(open) {
+   *  if (open) {
+   *    console.log('Tooltip is open')
+   *  }
+   * }
+   *
+   * <Tooltip onOpen={handleOpen}></Tooltip>
+   */
+  onOpen: PropTypes.func,
+}
+
+export default Tooltip
